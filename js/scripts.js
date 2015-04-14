80e57240d3b641732ece21a8f5fd0709f26682ea
shawntellemoncy <shawntellemoncy@gmail.com>
2015-04-13 21:17:59 -0700

    Base Commit

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// Media Element Player
+
+$('audio,video').mediaelementplayer({
+	success: function(player, node) {
+	  $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
+	},
+	startLanguage: 'en',
+	translationSelector: true
+  features: ['playpause','progress','current','volume'],
+
+});
+
+
+$('#video-filter').click(function(){
+
+  // hide audio
+  $('.mejs-audio').hide();
+  // show video
+  $('.mejs-video').show();
+  
+});
+
+$('#audio-filter').click(function(){
+  
+  // hide video
+  $('.mejs-video').hide();
+  // show video
+  $('.mejs-audio').show();
+
+});
